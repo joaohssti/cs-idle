cb46c35c47a5d86ab2e8ceabdb6d2c470f649659
João Henrique <joaohssti@gmail.com>
2025-10-29 19:20:24 -0300

    adicionados botões power,save,volume

--- cs-idle/scenes/about.py
+++ cs-idle/scenes/about.py
@@ -13,24 +13,16 @@ class AboutScene(BaseScene):
         self.title_surf = self.title_font.render(self.title_text,True, config.COR_BUTTON_TEXT)
         self.title_rect = self.title_surf.get_rect(center=(config.LAGRUGA_SCREEN//2, 200))
 
-        self.button = ImageButton(
-            image=pygame.image.load("cs-idle/assets/icons/power.png").convert_alpha(),
-            width=80, 
-            height=80,
-            center_pos=(100, 100), # Posição no canto da tela
-            action=None,
-            outline_thickness=3 # Um contorno um pouco mais espesso
-        )
-
     def handle_events(self, events):
-        for event in events:
-            self.button.handle_event(event)
-
+        super().handle_events(events)
+        
     def update(self):
         pass
 
     def draw(self, screen):
         
-        self.button.draw(screen)
+        screen.blit(self.title_surf, self.title_rect)
+
+        super().draw(screen)
 
         
--- cs-idle/scenes/base_scene.py
+++ cs-idle/scenes/base_scene.py
@@ -1,13 +1,45 @@
-
+import pygame
+from ui.image_button import ImageButton
 class BaseScene:
     def __init__(self, game):
         self.game = game
-    
+
+        # Botão Power
+        self.power_button = ImageButton(
+            image = pygame.image.load("cs-idle/assets/icons/power.png").convert_alpha(),
+            width=40, height=40,
+            center_pos=(50,50),
+            action= lambda: self.game.switch_scene("MAIN_MENU"),
+            outline_thickness=3
+        )
+
+        # Botão Save
+        self.save_button = ImageButton(
+            image=pygame.image.load("cs-idle/assets/icons/save.png").convert_alpha(),
+            width=40, height=40,
+            center_pos=(100, 50),
+            action= lambda: print("SAVE"),
+            outline_thickness=3
+        )
+        
+        # Botão Volume
+        self.volume_button = ImageButton(
+            image=pygame.image.load("cs-idle/assets/icons/volume.png").convert_alpha(),
+            width=40, height=40,
+            center_pos=(150, 50),
+            action= lambda: print("VOLUME"),
+            outline_thickness=3
+        )
+        self.common_ui_elements = [self.power_button, self.save_button, self.volume_button]
+
     def handle_events(self, events):
-        raise NotImplementedError
+        for event in events:
+            for button in self.common_ui_elements:
+                button.handle_event(event)
     
     def update(self):
         raise NotImplementedError
     
     def draw(self, screen):
-        raise NotImplementedError
+        for element in self.common_ui_elements:
+            element.draw(screen)
