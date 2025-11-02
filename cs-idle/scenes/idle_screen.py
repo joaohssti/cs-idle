183bcf1e4af294e86a5e841b9a33865e855ad536
João Henrique <joaohssti@gmail.com>
2025-11-01 22:36:00 -0300

    jogo dividido em frames, playtime e pontos para save, fix estado para newgame

--- cs-idle/scenes/idle_screen.py
+++ cs-idle/scenes/idle_screen.py
@@ -1,3 +1,4 @@
+import json
 import os
 import time
 import pygame
@@ -11,6 +12,11 @@ class IdleScreen(BaseScene):
     def __init__(self, game):
         super().__init__(game)
         
+        if self.game.current_save == "New Game":
+            with open("cs-idle/data/initial_game_state.json", 'r') as f:
+                self.game.save_data = json.load(f)
+                print("new game loaded ", self.game.save_data)
+
         # Divide a tela em 2 partes, com 1/3 e com 2/3
         self.screen_rect = self.game.screen.get_rect()
         self.left_frame = pygame.Rect(0, 0, self.screen_rect.width // 3, self.screen_rect.height)
@@ -20,8 +26,10 @@ class IdleScreen(BaseScene):
         self.base_font = pygame.font.Font("cs-idle/fonts/Orbitron/orbitron-light.otf", 18)
         self.buttons = []
 
-        # Botão do computador
-        computer_click_button = ImageButton(
+        self._build_computer()
+
+    def _build_computer(self):
+        self.computer_click_button = ImageButton(
             image=pygame.image.load("cs-idle/assets/parts/motherboard.png").convert_alpha(),
             width=256, height=256,
             center_pos=self.left_frame.center,
@@ -29,12 +37,27 @@ class IdleScreen(BaseScene):
             outline_thickness=3
         )
 
-        self.buttons.append(computer_click_button)
+    def _buil_left_frame_info(self):
+        self.left_frame_info = []
 
+        title_text = f"Save carregado: {self.game.current_save}"
+        title_surf = self.title_font.render(title_text,True, config.COR_BUTTON_TEXT)
+        title_rect = title_surf.get_rect(center=(self.left_frame.centerx, 150))
+        self.left_frame_info.append((title_surf, title_rect))
+
+        points_text = f"{utils.formatDataUnit(self.game.save_data['click_bytes'])}"
+        points_surf = self.base_font.render(points_text, True, config.COR_BUTTON_TEXT)
+        points_rect = points_surf.get_rect(center=(self.left_frame.centerx, 200))
+        self.left_frame_info.append((points_surf, points_rect))
+
+        timer_text = f"Tempo Jogado: {int(self.game.save_data['playtime'])}s"
+        timer_surf = self.base_font.render(timer_text, True, config.COR_BUTTON_TEXT)
+        timer_rect = timer_surf.get_rect(center=(self.left_frame.centerx, 250))
+        self.left_frame_info.append((timer_surf, timer_rect))
 
     def computer_click(self):
         click_bytes = self.game.save_data.get('click_bytes', 0.0)
-        click_bytes += 256
+        click_bytes += 1
         self.game.save_data['click_bytes'] = click_bytes
         print(self.game.save_data['click_bytes'])
 
@@ -43,6 +66,8 @@ class IdleScreen(BaseScene):
         for event in events:
             for button in self.buttons:
                 button.handle_event(event)
+            
+            self.computer_click_button.handle_event(event)
 
     def update(self):
         
@@ -58,18 +83,10 @@ class IdleScreen(BaseScene):
         for button in self.buttons:
             button.draw(screen)
 
-        self.title_text = f"Save carregado: {self.game.current_save}"
-        self.title_surf = self.title_font.render(self.title_text,True, config.COR_BUTTON_TEXT)
-        self.title_rect = self.title_surf.get_rect(center=(config.LAGRUGA_SCREEN//2, 200))
-        screen.blit(self.title_surf, self.title_rect)
+        self.computer_click_button.draw(screen)
 
-        points_text = f"{utils.formatDataUnit(self.game.save_data['click_bytes'])}s"
-        points_surf = self.base_font.render(points_text, True, config.COR_BUTTON_TEXT)
-        points_rect = points_surf.get_rect(center=(self.left_frame.centerx, 200))
-        screen.blit(points_surf, points_rect)
-        
-        timer_text = f"Tempo Jogado: {int(self.game.save_data['playtime'])}s"
-        timer_surf = self.base_font.render(timer_text, True, config.COR_BUTTON_TEXT)
-        timer_rect = timer_surf.get_rect(center=(config.LAGRUGA_SCREEN // 2, 240))
-        screen.blit(timer_surf, timer_rect)
+        self._buil_left_frame_info()
+
+        for surface, rect in self.left_frame_info:
+            screen.blit(surface, rect)
 
